9c975366c53f463fe0d6c08f713720711311d388
Kira366 <khaitran.k366@gmail.com>
2025-07-28 16:24:57 +0700

    add cva

---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,20 @@
+import { Menu } from "lucide-react";
+import logo from "../assets/Logo.png";
+import Button from "../components/Button";
+export default function PageHeader() {
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <Button size="icon" variant="ghost" >
+          <Menu />
+        </Button>
+        <a href="/">
+          <img src={logo} className="h-6" />
+        </a>
+      </div>
+
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
