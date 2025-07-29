7ac36ad2c42f21474a79b4ce48649ac76af7e2ae
Kira366 <khaitran.k366@gmail.com>
2025-07-29 11:41:01 +0700

    update

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,11 +1,12 @@
-import { Menu } from "lucide-react";
+import { Bell, Menu, Mic, Search, Upload, User } from "lucide-react";
 import logo from "../assets/Logo.png";
 import Button from "../components/Button";
 export default function PageHeader() {
   return (
-    <div className="flex gap-10 lg:gap-20 justify-between">
-      <div className="flex gap-4 items-center flex-shrink-0">
-        <Button size="icon" variant="ghost" >
+    <div className="flex gap-10 lg:gap-20 justify-between border pt-2 mb-6 mx-4">
+      {/* Logo And SideBar Button */}
+      <div className="flex gap-4 items-center flex-shrink-0 border-t border-r">
+        <Button size="icon" variant="ghost">
           <Menu />
         </Button>
         <a href="/">
@@ -13,8 +14,36 @@ export default function PageHeader() {
         </a>
       </div>
 
-      <div></div>
-      <div></div>
+      {/* Search Bar */}
+      <form className="flex flex-grow gap-4 justify-center">
+        <div className="flex flex-grow max-w-[600px] ">
+          <input
+            type="search"
+            placeholder="Search"
+            className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary w-full px-4 py-1 text-lg
+            focus:border-blue-500 outline-none"
+          />
+          <Button size="icon" className="bg-amber-500">
+            <Search />
+          </Button>
+        </div>
+        <Button size="icon" className="flex-shrink-0" type="button">
+          <Mic />
+        </Button>
+      </form>
+
+      {/* Right Section */}
+      <div className="flex flex-shrink-0 md:gap-2">
+        <Button variant="ghost" size="icon">
+          <Upload />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Bell />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <User />
+        </Button>
+      </div>
     </div>
   );
 }
